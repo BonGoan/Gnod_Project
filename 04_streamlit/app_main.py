4da584a718179f4748a6f8c23215f2b665d815a6
Nicole Pinto <med.insightz@gmail.com>
2024-10-11 16:15:07 +0200

    updated streamlit folder

---
+++ 04_streamlit/app_main.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pickle as pkl
+import os
+from functions import bring_song, classify_song, song_recommender, load_data, load_models
+from IPython.core.display import display, HTML
+import config
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= config.client_id,
+                                                           client_secret= config.client_secret))
+
+
+
+load_data ()
+load_models ()
+
+# Streamlit App
+def main():
+
+    st.title("Spotify Song Recommender")
+
+    # Menu options
+    options = ["Home", "Songs"]
+    choice = st.sidebar.selectbox("Menu", options, key='1')
+
+    if choice == 'Home':
+        st.text("🎶 Welcome to the Spotify Song Recommender! 🎶")
+        st.write("Simply navigate to the 'Songs' section and enter the name of your favorite song to discover new music!")
+        
+    elif choice == "Songs":
+        st.write("🎶 Find songs similar to your favorites! 🎶")
+        st.title("Choose the song")
+        song_name = st.text_input("Enter the song name")  
+        st.write("The song chosen is:", song_name)
+
+    
+
+        if song_name:
+            try:
+                st.write("Searching for similar songs...")
+            # Get song ID using the Spotify API
+                song_id = bring_song(song_name)
+
+            # Classify the song based on its audio features
+                pred_cluster = classify_song(song_id)
+
+            # Recommend similar songs from the playlist
+                recommended_songs = song_recommender(pred_cluster)
+            
+            
+
+            # Display the recommended songs
+                st.write("Recommended Songs:")
+                #st.write(recommended_songs['names'])
+                #st.write(display)
+
+            # Display the recommended songs with embedded players in a single column
+                for index, row in recommended_songs.iterrows():
+                    track_id = row['id']
+                    st.markdown(f"""
+                    <div style="margin: 10px; display: flex; flex-direction: column; align-items: center;">
+                        <iframe src="https://open.spotify.com/embed/track/{track_id}"
+                                width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                        <p style="text-align: center;">{row['names']}</p>
+                    </div>
+                    """, unsafe_allow_html=True)
+ 
+
+            # Display the recommended songs with a play button for each
+                #st.write("Recommended Songs:")
+                #for idx, (song_name, track_id) in enumerate(zip(recommended_songs['names'], recommended_songs['ids'])):
+                    #st.write(f"{idx + 1}. {song_name}")
+                    
+            # Create a button to play the song when clicked
+                #if st.button(f"Play {song_name}", key=f"play_{idx}"):
+                #play_song(track_id)  # Call play_song function with the track ID
+            
+        
+            except Exception as e:
+                st.error(f"Error: {e}")
+                st.write("Make sure the song name is valid and try again.")
+
+if __name__ == '__main__':
+    main()
---
+++ 04_streamlit/functions.py
@@ -0,0 +1,102 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import config
+import pandas as pd
+import spotipy
+import streamlit as st
+import pickle as pkl
+import joblib
+import os
+from IPython.core.display import display, HTML
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= config.client_id,
+                                                           client_secret= config.client_secret))
+
+
+def load_models():
+    global df_playlist_new, kmeans_model, scaler_model  # Declare as global to access in other functions
+
+    try:
+        # Load dataset
+        df_playlist_new = load_data()
+
+        # Load KMeans model
+        with open('kmeans.pkl', 'rb') as file:
+            kmeans_model = pkl.load(file)
+
+        # Load Scaler model
+        with open("scaler.pkl", 'rb') as file:
+            scaler_model = pkl.load(file)
+
+    except EOFError as e:
+        st.error(f"Error loading model: {e}")
+        st.stop()  # Stops the app execution
+
+
+
+
+
+def load_data():
+    file_path = os.path.join(os.path.dirname(__file__), '../01_data/df_playlist_new.csv')
+    try:
+        return pd.read_csv(file_path)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        raise
+
+
+def bring_song(song_name):
+    result = sp.search(q=song_name, limit=1, market="DE")
+    song_id = result['tracks']['items'][0]['id']  # Get the song ID
+    
+    return song_id
+
+
+def classify_song (selected_id):
+    features= sp.audio_features(selected_id)
+    song_features = pd.DataFrame(features)
+    song_features= song_features [["danceability", "energy", "loudness", "speechiness", "acousticness",
+                 "instrumentalness", "liveness", "valence", "tempo", "id", "duration_ms"]]
+    song_features= song_features.drop ("id", axis=1)
+    
+    pred_cluster = kmeans_model.predict(song_features)
+    
+    pred_cluster= pred_cluster[0]                          
+   
+    
+    return pred_cluster
+
+
+def song_recommender(pred_cluster):
+    same_cluster_songs = df_playlist_new.loc[df_playlist_new["cluster_knn"] == pred_cluster]
+    recommended_songs = same_cluster_songs.sample(n=3)
+    return recommended_songs
+    #{
+        #'names': recommended_songs['names'].tolist(),
+        #'ids': recommended_songs['id'].tolist()
+    #}
+
+def bring_track_id(recommended_songs):
+    track_id = recommended_songs['id'][0]
+    return track_id
+
+# Step 3: Play Song Function
+def play_song(track_id):
+    spotify_embed_code = f"""
+    <iframe src="https://open.spotify.com/embed/track/{track_id}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    """
+    outcome = display(HTML(spotify_embed_code))
+    return outcome
+
+
+
+# Play the first recommended song
+#track_id = recommended_songs['ids'][0]
+#embed_code = play_song(track_id)
+
+# Display the embed code (as HTML in Jupyter)
+#display(HTML(embed_code))
+
+#Bring track_id and show display
+#track_id_new = bring_track_id(recommended_songs)
+#display = play_song(track_id_new)
